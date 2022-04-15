5d8dbb141c53fab6091deafa847c06a356c62d20
Marcin Wróblewski <marcin.wroblewski@monterail.com>
2022-04-15 12:01:26 +0200

    Use restoration scope in auto_route

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -10,13 +10,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp.router(
-        routerDelegate: _router.delegate(),
+        routerDelegate: _router.delegate(
+          // Providing a navRestorationScopeId allows the Navigator built by the
+          // router to restore the navigation stack when a user leaves and
+          // returns to the app after it has been killed while running in the
+          // background.
+          navRestorationScopeId: 'app',
+        ),
         routeInformationParser: _router.defaultRouteParser(),
-        // Providing a restorationScopeId allows the Navigator built by the
-        // MaterialApp to restore the navigation stack when a user leaves and
-        // returns to the app after it has been killed while running in the
-        // background.
-        restorationScopeId: 'app',
 
         // Provide the generated AppLocalizations to the MaterialApp. This
         // allows descendant Widgets to display the correct translations
