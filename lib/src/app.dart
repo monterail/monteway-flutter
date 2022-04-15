f0190676a17129f9890dab85d599fbc3d20b7d67
Marcin Wróblewski <marcin.wroblewski@monterail.com>
2022-04-15 11:51:49 +0200

    Get rid of old comment

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -32,9 +32,6 @@ class MyApp extends StatelessWidget {
         onGenerateTitle: (BuildContext context) =>
             AppLocalizations.of(context)!.appTitle,
 
-        // Define a light and dark color theme. Then, read the user's
-        // preferred ThemeMode (light, dark, or system default) from the
-        // SettingsController to display the correct theme.
         theme: ThemeData(),
       );
 }
