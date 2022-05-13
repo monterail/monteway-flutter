2fa74f62523f690d341f99280db4beb8f520f4a3
Ruslan Zaripov <zaripo.rus@gmail.com>
2022-05-13 14:43:45 +0200

    [refactor] Bump dependencies, lint 2.0.1 changes

--- lib/src/modules/cubit_screen/cubit/counter_cubit.dart
+++ lib/src/modules/cubit_screen/cubit/counter_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:autoequal/autoequal.dart';
-import 'package:bloc/bloc.dart';
 import 'package:copy_with_extension/copy_with_extension.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'counter_cubit.g.dart';
 part 'counter_state.dart';
