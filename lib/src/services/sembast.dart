cb6968379fc53ff65a71996baaa3ab2013e0e19d
Michal Kochmanski <michal.kochmanski@monterail.com>
2025-02-14 10:52:21 +0100

    Add support for web (sembast_web)

--- lib/src/services/sembast.dart
+++ lib/src/services/sembast.dart
@@ -1,14 +1,22 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:sembast/sembast_io.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:sembast_web/sembast_web.dart';
 
+const String _databaseName = 'app_database.db';
 late Database _database;
 
 Future<void> setupSembast() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final appDir = await getApplicationDocumentsDirectory();
-  final dbPath = '${appDir.path}/app_database.db';
-  _database = await databaseFactoryIo.openDatabase(dbPath);
+  if (kIsWeb) {
+    // Web-specific database initialization
+    _database = await databaseFactoryWeb.openDatabase(_databaseName);
+  } else {
+    final appDir = await getApplicationDocumentsDirectory();
+    final dbPath = '${appDir.path}/databaseName';
+    _database = await databaseFactoryIo.openDatabase(dbPath);
+  }
 }
 
 Database get database => _database;
